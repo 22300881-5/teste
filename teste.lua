4ca8efd537dd33a4826ab135eb25848fde854d60
Silekz <145169330+22300881-5@users.noreply.github.com>
2025-08-08 12:45:23 -0300

    Update teste.lua

--- teste.lua
+++ teste.lua
@@ -1,18 +1,1 @@
-return function(webhookURL, message)
-    local HttpService = game:GetService("HttpService")
-
-    local data = { content = message }
-    local jsonData = HttpService:JSONEncode(data)
-
-    local headers = { ["Content-Type"] = "application/json" }
-
-    local success, response = pcall(function()
-        return HttpService:PostAsync(webhookURL, jsonData, Enum.HttpContentType.ApplicationJson, false, headers)
-    end)
-
-    if success then
-        print("✅ Mensagem enviada com sucesso!")
-    else
-        warn("❌ Erro ao enviar mensagem: " .. tostring(response))
-    end
-end
+print("Testando...")
