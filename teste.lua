0bcf47049a7c180466527d98ec17093399b2a489
Silekz <145169330+22300881-5@users.noreply.github.com>
2025-08-12 13:05:54 -0300

    Update teste.lua

--- teste.lua
+++ teste.lua
@@ -1,6 +1,7 @@
  local player = game.Players.LocalPlayer
     local playerGui = player:WaitForChild("PlayerGui")
 
+local loadTries = 0
 local function loadChar()
     local PreloadUI = playerGui:WaitForChild("Preload") -- nome da sua ScreenGui
     local PirateButton = PreloadUI:WaitForChild("ChooseTeam").PirateButton -- nome do botão
@@ -10,12 +11,11 @@ local function loadChar()
     for _, connection in pairs(getconnections(PirateButton.MouseButton1Click)) do
         connection.Function() -- chama o que estaria no clique
     end 
-    print("Player entrou no jogo!")
+    print(loadTries.."x tentando dar play...")
 end
 
 while not playerGui:FindFirstChild("Cmdr") or playerGui:FindFirstChild("Cmdr") do
-    print("Ainda não deu play!")
     loadChar()
     task.wait(1)
 end
-print("no jogo!")
+print("Deu play!")
