b089cb5c9c2f87804bc3a31bd159ca351c845ebb
Silekz <145169330+22300881-5@users.noreply.github.com>
2025-08-12 12:54:10 -0300

    Update teste.lua

--- teste.lua
+++ teste.lua
@@ -1,2 +1,33 @@
+local function loadChar()
+    -- LocalScript dentro de StarterPlayerScripts
+    local player = game.Players.LocalPlayer
+    local playerGui = player:WaitForChild("PlayerGui")
 
-print("Testando...")
+    -- Espera a GUI carregar
+    local screenGui = playerGui:WaitForChild("Preload") -- nome da sua ScreenGui
+    local playButton = screenGui:WaitForChild("ChooseTeam").PirateButton -- nome do botão
+
+    -- Simula clique assim que entrar
+    task.wait(1) -- pequeno atraso para garantir que tudo carregou
+    for _, connection in pairs(getconnections(playButton.MouseButton1Click)) do
+        connection.Function() -- chama o que estaria no clique
+    end 
+    print("Player entrou no jogo!")
+end
+
+local player = game.Players.LocalPlayer
+local playerGui = player:WaitForChild("PlayerGui")
+
+-- Espera a GUI carregar
+local screenGui = playerGui:FindFirstChild("Preload") -- nome da sua ScreenGui
+if not screenGui then
+    print("Já clicou em play!")
+else
+    local playButton = screenGui:WaitForChild("ChooseTeam").PirateButton -- nome do botão
+    while not playButton do
+        task.wait(1)
+    end
+print("Botões carregaram!")
+end
+
+loadChar()
