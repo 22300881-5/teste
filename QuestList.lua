9b0dac2ccaa496eadbc0b8d91de104b9fa755920
Silekz <145169330+22300881-5@users.noreply.github.com>
2026-08-20 16:15:30 -0300

    Create QuestList.lua

---
+++ QuestList.lua
@@ -0,0 +1,82 @@
+return {
+    {lvl = 2250, Name = "Kill 4 Beast Pirates", NPC = "Beast Pirate"},
+    {lvl = 2300, Name = "Kill 4 Beast Swordman", NPC = "Beast Swordman"},
+    {lvl = 2350, Name = "Kill 1 Gazelle Man", NPC = "Gazelle Man"},
+
+    {lvl = 2400, Name = "Kill 4 Bandit Beast Pirates", NPC = "Bandit Beast Pirate"},
+    {lvl = 2450, Name = "Kill 4 Powerful Beast Pirates", NPC = "Powerful Beast Pirate"},
+    {lvl = 2500, Name = "Kill 1 Violet Samurai", NPC = "Violet Samurai"},
+
+    {lvl = 2550, Name = "Kill 1 Duke", NPC = "Duke"},
+    {lvl = 2600, Name = "Kill 1 Magician", NPC = "Magician"},
+    {lvl = 2650, Name = "Kill 1 Kitsune Samurai", NPC = "Kitsune Samurai"},
+    
+    {lvl = 2700, Name = "Kill 4 Elite Beast Pirates", NPC = "Elite Beast Pirate"},
+    {lvl = 2750, Name = "Kill 1 Bear Man", NPC = "Bear Man"},
+    {lvl = 2800, Name = "Kill 1 Bean", NPC = "Bean"},
+
+    {lvl = 2850, Name = "Kill 1 Meji", NPC = "Meji"},
+    {lvl = 2900, Name = "Kill 1 Petra", NPC = "Petra"},
+
+    {lvl = 2950, Name = "Kill 1 Kappa", NPC = "Kappa"},
+    {lvl = 3000, Name = "Kill 1 Joey", NPC = "Joey"},
+
+    --//////////////
+
+    {lvl = 3025, Name = "Kill 4 Skull Pirates", NPC = "Skull Pirate"},
+    {lvl = 3075, Name = "Kill 1 Elite Skeleton", NPC = "Elite Skeleton"},
+
+    --//////////////
+
+    {lvl = 3100, Name = "Kill 1 Desert Thief", NPC = "Desert Thief"},
+    {lvl = 3125, Name = "Kill 1 Anubis", NPC = "Anubis"},
+    {lvl = 3150, Name = "Kill 1 Pharaoh", NPC = "Pharaoh"},
+    {lvl = 3175, Name = "Kill 1 Flame User", NPC = "Flame User"},
+        
+    --//////////////
+
+    {lvl = 3200, Name = "Kill 4 Chess Soldiers", NPC = "Chess Soldier"},
+    {lvl = 3225, Name = "Kill 1 Sunken Vessel", NPC = "Sunken Vessel"},
+    {lvl = 3250, Name = "Kill 1 Biscuit Man", NPC = "Biscuit Man"},
+
+    {lvl = 3275, Name = "Kill 1 Dough Master", NPC = "Dough Master"},
+
+    --//////////////
+
+    {lvl = 3300, Name = "Kill 4 Azlan", NPC = "Azlan"},
+    {lvl = 3325, Name = "Kill 4 The Volcano", NPC = "The Volcano"},
+   -- {lvl = 3275, Name = "Kill 1 The Ice Master", NPC = "Dough Master"}, ** precisa de item pra spwanar, jogo imbecil
+    --{lvl = 3275, Name = "Kill 1 The Lava Master", NPC = "Dough Master"},
+
+    --//////////////
+
+    {lvl = 3400, Name = "Kill 4 Dark Beard Servant", NPC = "Dark Beard Servant"},
+    {lvl = 3425, Name = "Kill 1 Supreme Swordman", NPC = "Supreme Swordman"},
+    {lvl = 3450, Name = "Kill 1 Sally", NPC = "Sally"},
+    --{lvl = 3450, Name = "Kill 1 Dark Beard", NPC = "Dark Beard"},** precisa de item pra spwanar, jogo imbecil
+
+    --//////////////
+
+    {lvl = 3500, Name = "Kill 5 Vice Admiral", NPC = "Vice Admiral"},
+    {lvl = 3525, Name = "Kill 1 Pondere", NPC = "Pondere"},
+
+    {lvl = 3550, Name = "Kill 1 Hefty", NPC = "Hefty"},
+    --{lvl = 3575, Name = "Kill 1 Lucidus", NPC = "Lucidus"},** precisa de item pra spwanar, jogo imbecil
+
+
+    {lvl = 3600, Name = "Kill 6 Fiore Gladiator", NPC = "Fiore Gladiator"},
+    {lvl = 3625, Name = "Kill 6 Fiore Fighter", NPC = "Fiore Fighter"},
+
+    {lvl = 3650, Name = "Kill 7 Fiore Pirate", NPC = "Fiore Pirate"},
+    {lvl = 3675, Name = "Kill 1 Lomeo", NPC = "Lomeo"},
+
+    {lvl = 3700, Name = "Kill 1 Prince Aria", NPC = "Prince Aria"},
+    {lvl = 3725, Name = "Kill 1 Devastate", NPC = "Devastate"},
+    {lvl = 3750, Name = "Kill 1 Physicus", NPC = "Physicus"},
+    {lvl = 3775, Name = "Kill 1 Floffy", NPC = "Floffy"},
+
+    {lvl = 3800, Name = "Kill 4 Dead Troupe", NPC = "Dead Troupe"},
+    {lvl = 3850, Name = "Kill 4 Dead Troupe Captain", NPC = "Dead Troupe Captain"},
+    {lvl = 3950, Name = "Kill 1 Ryu", NPC = "Ryu"},
+
+}
