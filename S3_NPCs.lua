c2d6b7ac68766d819b9dc6a8f9ba5d7bddc52fee
Silekz <145169330+22300881-5@users.noreply.github.com>
2026-09-04 08:56:18 -0300

    Create S3_NPCs.lua

---
+++ S3_NPCs.lua
@@ -0,0 +1,14 @@
+return {
+    ["Soldier"] = {
+        Location = CFrame.new(-4311,57,40),
+        Folder_Path = workspace.Monster.Mon
+    },
+    ["Clown Pirate"] = {
+        Location = CFrame.new(-4311,57,40),
+        Folder_Path = workspace.Monster.Mon
+    },
+    ["Gazelle Man"] = {
+        Location = CFrame.new(-4311,57,40),
+        Folder_Path = workspace.Monster.Boss
+    },
+}
