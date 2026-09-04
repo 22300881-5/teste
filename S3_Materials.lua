f7dc7bceba1cb0c7c1d9f89e560c3798b7c1d5ae
Silekz <145169330+22300881-5@users.noreply.github.com>
2026-09-04 08:51:46 -0300

    Update S3_Materials.lua

--- S3_Materials.lua
+++ S3_Materials.lua
@@ -1,1 +1,37 @@
+local MonsterFolder = workspace:WaitForChild("Monster")
 
+local Data = {
+   ["Sea Artifact"] = {
+        {
+            Path = MonsterFolder.Mon,
+            Location = CFrame.new(3467,203,915),
+            NPC = "Deep one Villager [Lv. 4100]",
+        },
+        {
+            Path = MonsterFolder.Mon,
+            Location = CFrame.new(1568,35,965),
+            NPC = "Deep Diver [Lv. 4000]",
+        },
+            {
+            Path = MonsterFolder.Mon,
+            Location = CFrame.new(3467,203,915),
+            NPC = "Deep one Villager [Lv. 4100]",
+        },
+    },
+    ["Shark Fin"] = {
+        {
+            Path = MonsterFolder.Mon,
+            Location = CFrame.new(1763,66,206),
+            NPC = "Fishman Guardian [Lv. 4150]",
+        },
+    },
+    ["Coral"] = {
+        {
+            Path = MonsterFolder.Boss,
+            Location = CFrame.new(2838,35,1114),
+            NPC = "Fugitive [Lv. 4050]",
+        },
+    },
+}
+
+return Data
