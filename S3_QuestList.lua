d9740d141ffec8c77420623251e59d4c792a9ed5
Silekz <145169330+22300881-5@users.noreply.github.com>
2026-09-04 08:53:45 -0300

    Create S3_QuestList.lua

---
+++ S3_QuestList.lua
@@ -0,0 +1,50 @@
+return {
+    {lvl = 4000, Name = "Kill 4 Deep Diver", NPC = "Deep Diver"},
+    {lvl = 4050, Name = "Kill 1 Fugitive", NPC = "Fugitive"},
+    {lvl = 4100, Name = "Kill 4 Deep one Villager", NPC = "Deep one Villager"},
+
+    {lvl = 4150, Name = "Kill 6 Fishman Guardian", NPC = "Fishman Guardian"},
+    {lvl = 4200, Name = "Kill The deep one", NPC = "The deep one"},
+    {lvl = 4250, Name = "Kill Fishman King's Guard", NPC = "Fishman King's Guard"},
+
+
+
+    {lvl = 4300, Name = "Kill 5 Jungle Gorilla", NPC = "Jungle Gorilla"},
+    {lvl = 4325, Name = "Kill 5 Wilderness Gorilla", NPC = "Wilderness Gorilla"},
+    {lvl = 4350, Name = "Kill 5 Jungle Ape", NPC = "Jungle Ape"},
+    {lvl = 4375, Name = "Kill 1 Cyborg Gorilla", NPC = "Cyborg Gorilla"},
+
+
+    {lvl = 4400, Name = "Kill 1 Ripcurrent Raider", NPC = "Ripcurrent Raider"},
+    {lvl = 4450, Name = "Kill 1 Tidal Warrior", NPC = "Tidal Warrior"},
+    {lvl = 4500, Name = "Kill 1 Ocean Gladiator", NPC = "Ocean Gladiator"},
+
+    {lvl = 4550, Name = "Kill 4 Deepfire Combatant", NPC = "Deepfire Combatant"},
+    {lvl = 4600, Name = "Kill 1 Electro Abyss Warrior", NPC = "Electro Abyss Warrior"},
+    {lvl = 4650, Name = "Kill 1 Inferno Diver", NPC = "Inferno Diver"},
+
+    {lvl = 4700, Name = "Kill 1 Tempest Tidebreaker", NPC = "Tempest Tidebreaker"},
+    {lvl = 4750, Name = "Kill 1 Abyss Swordsman", NPC = "Abyss Swordsman"},
+
+
+
+    {lvl = 4800, Name = "Kill 4 Rogue Prisoner", NPC = "Rogue Prisoner"},
+    {lvl = 4850, Name = "Kill 4 Prisoner Buccaneer", NPC = "Prisoner Buccaneer"},
+    {lvl = 4875, Name = "Kill 1 Prisoner of Gravity", NPC = "Prisoner of Gravity"},
+
+
+        {lvl = 4875, Name = "Kill 1 Prisoner of Gravity", NPC = "Prisoner of Gravity"},
+
+
+
+    {lvl = 5000, Name = "Kill 8 Obsidian Seeker", NPC = "Obsidian Seeker"},
+    {lvl = 5050, Name = "Kill 8 Forgotten Delver", NPC = "Forgotten Delver"},
+    {lvl = 5100, Name = "Kill 8 Blackreach Scout", NPC = "Blackreach Scout"},
+    {lvl = 5150, Name = "Kill 1 Nightbound Explorer", NPC = "Nightbound Explorer"},
+
+    {lvl = 5200, Name = "Kill 8 Ruinstep Nomad", NPC = "Ruinstep Nomad"},
+    {lvl = 5250, Name = "Kill 1 Ancient Wayfarer", NPC = "Ancient Wayfarer"},
+    {lvl = 5300, Name = "Kill 8 Depths Voyager", NPC = "Depths Voyager"},
+    {lvl = 5350, Name = "Kill 1 Allosaurus", NPC = "Allosaurus"},
+    {lvl = 5400, Name = "Kill 1 Spinosaurus", NPC = "Spinosaurus"},
+}
